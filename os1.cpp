2cb897690f08227aaf705742962aacd92768b801
sunsupkri <49608218+sunsupkri@users.noreply.github.com>
2019-04-14 21:28:04 +0530

    Create os1.cpp

---
+++ os1.cpp
@@ -0,0 +1,17 @@
+#include<conio.h>
+#include<stdio.h>
+int main()
+{
+ 	int n;   
+ 	int r;      
+ 	int i,j,k;
+ 	int need[10][10],alloc[10][10],max[10][10];
+ 	int avail[10],p[10];
+  	printf("\nEnter number of process :");
+ 	scanf("%d",&n);
+ 	printf("\n Enter  resources available : ");
+ 	scanf("%d",&r);
+ 	
+ 	
+ 	
+}
